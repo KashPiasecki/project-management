dbef4564b3d4113d4aaad1222b8d537ae8c52ac1
Lukasz Piasecki <kash.piasecki92@gmail.com>
2020-10-14 12:20:39 +0200

    Add 1st entity, view and controller.

---
+++ src/main/java/pl/kashp/projectmanagement/controllers/ProjectController.java
@@ -0,0 +1,26 @@
+package pl.kashp.projectmanagement.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import pl.kashp.projectmanagement.entities.Project;
+
+@Controller
+@RequestMapping("/projects")
+public class ProjectController {
+
+
+    @GetMapping("/new")
+    public String displayProjectForm(Model model){
+        Project aProject = new Project();
+        model.addAttribute("project", aProject);
+        return "new-project";
+    }
+
+    @PostMapping("/save")
+    public String createProject(Model model, Project project){
+        // Should handle saving to the database
+    }
+}
