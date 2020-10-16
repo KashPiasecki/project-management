9437d7854826bf3d035d9e86518af54362c95755
Lukasz Piasecki <kash.piasecki92@gmail.com>
2020-10-16 13:43:03 +0200

    Add CascadeType and Fetch to child dependence (Employee). Add Feature to add employees in to a project.

--- src/main/java/pl/kashp/projectmanagement/controllers/ProjectController.java
+++ src/main/java/pl/kashp/projectmanagement/controllers/ProjectController.java
@@ -6,7 +6,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import pl.kashp.projectmanagement.dao.EmployeeRepository;
 import pl.kashp.projectmanagement.dao.ProjectRepository;
+import pl.kashp.projectmanagement.entities.Employee;
 import pl.kashp.projectmanagement.entities.Project;
 
 import java.util.List;
@@ -16,22 +19,31 @@ import java.util.List;
 public class ProjectController {
 
     private ProjectRepository proRepo;
+    private EmployeeRepository empRepo;
 
     @Autowired
-    public ProjectController(ProjectRepository proRepo) {
+    public ProjectController(ProjectRepository proRepo, EmployeeRepository epmRepo) {
         this.proRepo = proRepo;
+        this.empRepo = epmRepo;
     }
 
     @GetMapping("/new")
     public String displayProjectForm(Model model){
         Project aProject = new Project();
         model.addAttribute("project", aProject);
+        List<Employee> employees = empRepo.findAll();
+        model.addAttribute("allEmployees", employees);
         return "/projects/new-project";
     }
 
     @PostMapping("/save")
-    public String createProject(Model model, Project project){
+    public String createProject(Model model, Project project, @RequestParam List<Long> employees){
         proRepo.save(project);
+        Iterable<Employee> choosenEmployees = empRepo.findAllById(employees);
+        for (Employee emp : choosenEmployees) {
+            emp.setProject(project);
+            empRepo.save(emp);
+        }
         //use a redirect to prevent duplicate submissions
         return "redirect:/projects/new";
     }
